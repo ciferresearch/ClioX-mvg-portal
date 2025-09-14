21010c501a71311fc4dda71f3c966bb00a05e5a3
Binming Li <binming.li@outlook.com>
2025-09-14 03:57:10 -0700

    refactor(use cases): update names and remove test chatbot component
    - Renamed "Email Text Analysis Visualization" to "Email Text Visual Analysis" and "Cameroon Gazette Text Analysis Visualization" to "Cameroon Gazette Visual Analysis" for consistency.
    - Removed the ChatbotIris component and its associated files as it is no longer in use.
    - Updated the use cases menu to reflect the changes in naming.
    
    Signed-off-by: Binming Li <binming.li@outlook.com>

--- src/components/ChatbotIris/_constants.ts
+++
@@ -1,6 +0,0 @@
-export const IRIS_CHATBOT_ALGO_DIDS: Record<number, string> = {
-  32456:
-    'did:op:ae86a4a2fa73e9e24f9bbae97fa6ca8033f1b0891a155db5aada92e618e0fed7'
-}
-
-export const CHATBOT_NAMESPACE = 'chatbot:iris'
