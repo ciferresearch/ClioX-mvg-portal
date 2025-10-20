80caec3cacc10240481751bbed80d67edceb32df
Binming Li <binming.li@outlook.com>
2025-10-20 15:45:59 -0700

    feat(chatbot): support multiple algorithm DIDs; add Intepares General DID
    - Accept string | string[] in algoDidsByChain and flatten for filtering (Chatbot/JobList)
    - Add `did:op:b8bdaa459f5ad60d22e498c6175f86c239873eacb40f23527f63b2feed1092fa` to `INTEPARES_GENERAL_CHATBOT_ALGO_DIDS` on chain `32456`
    - Backwards compatible with single-DID configs
    - Type check passes
    
    Signed-off-by: Binming Li <binming.li@outlook.com>

--- src/components/ChatbotInteparesGeneral/_constants.ts
+++ src/components/ChatbotInteparesGeneral/_constants.ts
@@ -1,6 +1,11 @@
-export const INTEPARES_GENERAL_CHATBOT_ALGO_DIDS: Record<number, string> = {
-  32456:
-    'did:op:d19cbfb219315e75ac7a1504084df860d822965db568e201a8d84b7bc8ada43d'
+export const INTEPARES_GENERAL_CHATBOT_ALGO_DIDS: Record<
+  number,
+  string[] | string
+> = {
+  32456: [
+    'did:op:d19cbfb219315e75ac7a1504084df860d822965db568e201a8d84b7bc8ada43d',
+    'did:op:b8bdaa459f5ad60d22e498c6175f86c239873eacb40f23527f63b2feed1092fa'
+  ]
 }
 
 export const CHATBOT_NAMESPACE = 'chatbot:intepares-general'
