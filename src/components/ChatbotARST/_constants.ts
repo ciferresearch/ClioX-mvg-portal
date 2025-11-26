6184000d8f4390e7d47dd436c1f412bd47c81402
Binming Li <binming.li@outlook.com>
2025-11-26 03:18:01 -0800

    fix(chatbot): update ARST chatbot algorithm DID to a valid value
    Signed-off-by: Binming Li <binming.li@outlook.com>

--- src/components/ChatbotARST/_constants.ts
+++ src/components/ChatbotARST/_constants.ts
@@ -1,5 +1,6 @@
 export const ARST_CHATBOT_ALGO_DIDS: Record<number, string | string[]> = {
-  32456: 'did:op:TODO_FILL_LATER'
+  32456:
+    'did:op:8d7ea318e9d663bdc23971552e4d1489da0f1ff17e59328ef1cd85abb1ad26b2'
 }
 
 export const CHATBOT_NAMESPACE = 'chatbot:arst'
