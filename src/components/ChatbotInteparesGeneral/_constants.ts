49dfcccc719ea7e07a2573b5479a1fb8c100e6a5
Binming Li <binming.li@outlook.com>
2026-04-10 06:59:59 -0700

    refactor: align algo DID allowlists with paired use case algos

--- src/components/ChatbotInteparesGeneral/_constants.ts
+++ src/components/ChatbotInteparesGeneral/_constants.ts
@@ -3,10 +3,7 @@ export const INTEPARES_GENERAL_CHATBOT_ALGO_DIDS: Record<
   string[] | string
 > = {
   32456: [
-    'did:op:d19cbfb219315e75ac7a1504084df860d822965db568e201a8d84b7bc8ada43d',
-    'did:op:b8bdaa459f5ad60d22e498c6175f86c239873eacb40f23527f63b2feed1092fa',
-    'did:op:bb83fe22d731132cde6c576cf1d0eb4c5746c9277dd60831039168e9d0237552',
-    'did:op:5404dfc36448fb76f2bca918a05c67edb2f2d852e205862abbba2463b5916eba'
+    'did:op:bb83fe22d731132cde6c576cf1d0eb4c5746c9277dd60831039168e9d0237552'
   ]
 }
 
